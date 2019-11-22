8dfee3475b27dab6d6817f4e904ac06fc02cd837
lihqi <455711093@qq.com>
2019-11-22 09:22:18 +0000

    Deploying to gh-pages from master fbb56a604e27a57f056be8605d1d939f3086ca4b

---
+++ precache-manifest.7b2ad75d4358eb25c9c22ee2f26e8960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8945820727855f5e065a9bcf2bb4bf57",
+    "url": "/test-action/index.html"
+  },
+  {
+    "revision": "406db311dd07ada1622c",
+    "url": "/test-action/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "076cd77fa42ecde5144f",
+    "url": "/test-action/static/js/2.b8237891.chunk.js"
+  },
+  {
+    "revision": "406db311dd07ada1622c",
+    "url": "/test-action/static/js/main.90441a67.chunk.js"
+  },
+  {
+    "revision": "a1254c52c4679d40ef60",
+    "url": "/test-action/static/js/runtime-main.6231af14.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test-action/static/media/logo.25bf045c.svg"
+  }
+]);
